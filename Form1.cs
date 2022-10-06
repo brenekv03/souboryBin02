4d7f62ca6656b654d5f8f6e0075ba3b99ebc62d9
brenekv.03 <brenekv.03@A321-15>
2022-10-06 11:04:09 +0200

    Hotovo

--- Form1.cs
+++ Form1.cs
@@ -21,8 +21,7 @@ namespace souboryBin02
         private void button1_Click(object sender, EventArgs e)
         {
             int n = int.Parse(textBox1.Text);
-            FileStream fs = new FileStream(@"..\..\realnaCisla.dat", FileMode.Open, FileAccess.ReadWrite);
-            BinaryReader br = new BinaryReader(fs);
+            FileStream fs = new FileStream(@"..\..\realnaCisla.dat", FileMode.Open, FileAccess.Write);
             BinaryWriter bw = new BinaryWriter(fs);
             Random random = new Random();
             for(int i =0;i<n;i++)
@@ -30,12 +29,35 @@ namespace souboryBin02
                 double cislo = random.NextDouble()*(200-(-200))-200;
                 bw.Write(cislo);
             }
-            while(fs.Position < fs.Length)
+            fs.Close();
+            FileStream fs1 = new FileStream(@"..\..\realnaCisla.dat", FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs1);
+            while(br.BaseStream.Position<br.BaseStream.Length)
             {
                 double soubor = br.ReadDouble();
                 listBox1.Items.Add(soubor);
             }
             fs.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            FileStream fs = new FileStream(@"..\..\realnaCisla.dat",FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            StreamWriter streamWriter = new StreamWriter("realnaCisla.txt");
+            while(fs.Position<fs.Length)
+            {
+                double cislo = br.ReadDouble();
+                streamWriter.WriteLine(cislo);
+            }
+            fs.Close();
+            streamWriter.Close();
+            StreamReader streamReader = new StreamReader("realnaCisla.txt");
+            while(!streamReader.EndOfStream)
+            {
+                listBox1.Items.Add(streamReader.ReadLine());
+            }
         }
     }
 }
